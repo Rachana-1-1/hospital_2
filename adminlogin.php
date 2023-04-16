6c0b1baa4daf5077e5cc8608c55ad5c65b4205ef
Rachana-1-1 <20071A3207@vnrvjiet.in>
2023-04-16 19:06:26 +0530

    checking commit changes

--- adminlogin.php
+++ adminlogin.php
@@ -13,7 +13,6 @@ $password  = mysqli_real_escape_string($con,$_POST['password']);
 $res = mysqli_query($con,"SELECT * FROM doctor WHERE doctorId = '$doctorId'");
 
 $row=mysqli_fetch_array($res,MYSQLI_ASSOC);
-// echo $row['password'];
 if ($row['password'] == $password)
 {
 $_SESSION['doctorSession'] = $row['doctorId'];
